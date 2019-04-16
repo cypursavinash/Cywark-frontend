7d14e17c4b363cc4dc853f25dc3b8b8455699c45
Tushar Bhattacharya <tusharb.10590@gmail.com>
2019-04-16 13:22:59 +0530

    Testing routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule,Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { RegComponent } from './reg/reg.component';
 import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path:'register',component:RegComponent},
+  {path:'login',component:LoginComponent}
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +19,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
