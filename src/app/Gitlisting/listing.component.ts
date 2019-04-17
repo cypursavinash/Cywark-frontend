bb50211d4d47b86a88eb13a6db58e9a7d86a005f
cypurs-priyanshu <48239280+cypurs-priyanshu@users.noreply.github.com>
2019-04-17 11:04:37 +0530

    =ts files

---
+++ src/app/Gitlisting/listing.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listing',
+  templateUrl: './listing.component.html',
+  styleUrls: ['./listing.component.css']
+})
+export class ListingComponent  implements OnInit {
+
+  constructor() { } 
+
+  ngOnInit() {
+  }
+    myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
+}
