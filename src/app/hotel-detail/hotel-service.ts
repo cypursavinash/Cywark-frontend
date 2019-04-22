2f339ce2b2676e0674dd3030b26335ad16826b92
cypurs_priyanshu <priyanshu.cypurs@gmail.com>
2019-04-22 16:38:12 +0530

    =22-04

---
+++ src/app/hotel-detail/hotel-service.ts
@@ -0,0 +1,19 @@
+// import{ Injectable} from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// @Injectable()
+// export class Employeeservice {
+//      private _url:string="./json_data/json.data";
+
+
+//     constructor(private http: HttpClient) {
+
+//         getEmloyees(){
+
+
+//             return  this.http.get(this._url);
+//         }
+ 
+
+//     }
+
+// }
