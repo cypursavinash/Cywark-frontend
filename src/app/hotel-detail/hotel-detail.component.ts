d59e9ff6dc1e510301430974ddef781d7834097d
cypurs_priyanshu <priyanshu.cypurs@gmail.com>
2019-04-24 18:13:25 +0530

    =24-04 dynamic code of hotel-detail

--- src/app/hotel-detail/hotel-detail.component.ts
+++ src/app/hotel-detail/hotel-detail.component.ts
@@ -9,13 +9,35 @@ export class HotelDetailComponent implements OnInit {
 
   navItems: any;
   // constructor(private http: Http) { }
-
+  locationName: string;
+  locationState:string = "Gujarat";
+  locationCity:string = "Ahmedabad";
+  locationPlace : string;
+  offersList:Array<any>;
   ngOnInit() {
     this.loadNavItems();
-  }
+     
+    this.init();
+    this.offersList = []
+
+
+    this.offersList = [
+      {"styleClass":"fa fa-snowflake-o","label":"Ac Area"},
+      {"styleClass":"fa fa-plug","label":"Plugs"},
+      {"styleClass":"fa fa-shower","label":"Toilet"}
+    ]
+   }
+  
 
   loadNavItems() {
     // this.navItems = this.http.get("../json_data/data.json");
     console.log(this.navItems);
+}
+ 
+
+init(){
+this.locationName = "Royal plaza"
+this.locationPlace = this.locationCity;//+" , "+this.locationState;
+this.offersList = []
 }
 }
