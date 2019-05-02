3bbdc8f9deb7eaf2566c6f9320eb7205b8cfa961
cypurs-priyanshu <48239280+cypurs-priyanshu@users.noreply.github.com>
2019-05-02 19:21:03 +0530

    Add files via upload
    ionic 4 google login android

---
+++ tab2.module.ts
@@ -0,0 +1,20 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab2Page } from './tab2.page';
+import  { GoogleLoginComponent } from '../comps/google-login/google-login.component';
+ @NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: '', component: Tab2Page }])
+  ],
+  declarations: [
+    Tab2Page,
+    GoogleLoginComponent
+  ]
+})
+export class Tab2PageModule {}
